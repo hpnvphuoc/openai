4beb24aeb5f357720fa78eab0e650d5017232e45
Nguyen Van Phuoc <nvphuoc@sk-global.biz>
2024-05-14 09:41:50 +0700

    Change OpenAIChatCompletionChoiceMessageContentItemModel type from image_base64 to image_url

--- lib/src/core/models/chat/sub_models/choices/sub_models/sub_models/content.dart
+++ lib/src/core/models/chat/sub_models/choices/sub_models/sub_models/content.dart
@@ -59,7 +59,7 @@ class OpenAIChatCompletionChoiceMessageContentItemModel {
     String imageBase64,
   ) {
     return OpenAIChatCompletionChoiceMessageContentItemModel._(
-      type: 'image_base64',
+      type: 'image_url',
       imageBase64: imageBase64,
     );
   }
@@ -71,7 +71,7 @@ class OpenAIChatCompletionChoiceMessageContentItemModel {
       if (text != null) "text": text,
       if (imageUrl != null) "image_url": imageUrl,
       if (imageBase64 != null)
-        "image_url": {"url": "data:image/jpeg;base64,${imageBase64}"}
+        "image_url": {"url": "data:image/jpeg;base64,${imageBase64}"},
     };
   }
 
